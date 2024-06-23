a66b334e60e0c036aabfb7276f3a709c6c066e35
victor.ferrari <victor.ferrari@pto.org.br>
2024-06-23 20:47:34 -0300

    vercel

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [vue()],
-  base: "./", // ou o caminho base correto se estiver em um subdiretório
+  plugins: [react()],
+  base: "/",
 });
